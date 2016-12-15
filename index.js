32564220628d7d2151def6cff3d8dc74bc5b0aa3
samuraime <samurai7@foxmail.com>
2016-12-15 16:24:40 +0800

    add examples server

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const Koa = require('koa');
 const serve = require('koa-static');
-const router = require('koa-router')();
+const router = require('koa-router');
+const path = require('path');
+const serveIndex = require('koa-serve-index');
 
 const app = new Koa();
 
-router.get('/examples', (ctx, next) => {
-  ctx.body = 'example list';
-});
-
-app.use(router.routes());
-
-app.use(serve(__dirname + '/public'));
+const publicDir = path.join(__dirname, 'public');
+app.use(serveIndex(publicDir));
+app.use(serve(publicDir));
 
 app.listen(3000);
